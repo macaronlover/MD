3d5e7e7f6bda34f5dadc9840dd5670c53a46c87d
macaronlover <macaronlover22@gmail.com>
2020-07-14 16:26:27 +0100

    Update MainPage.xaml.cs

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -15,18 +15,22 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace MD
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+
+            Binding myBinding = new Binding()
+            {
+                Source = Emails,
+                Mode = BindingMode.OneWay
+            };
+
+            MyMasterDetailsView.SetBinding(MasterDetailsView.ItemsSourceProperty, myBinding);
 
             var emails = MyEmailManager.GetEmails();
             emails.ForEach(email => Emails.Add(email));
